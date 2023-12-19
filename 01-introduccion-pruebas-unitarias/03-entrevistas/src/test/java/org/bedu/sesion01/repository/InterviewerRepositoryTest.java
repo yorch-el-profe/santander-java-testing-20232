f2c8188b0616c4772f8fe6b93efae994ccc8fdd9
Jorge Ramón <jorge.ramon@autodesk.com>
2023-12-18 21:05:36 -0600

    Ejemplo 03: Pruebas unitarias del repositorio

--- 01-introduccion-pruebas-unitarias/03-entrevistas/src/test/java/org/bedu/sesion01/repository/InterviewerRepositoryTest.java
+++ 01-introduccion-pruebas-unitarias/03-entrevistas/src/test/java/org/bedu/sesion01/repository/InterviewerRepositoryTest.java
@@ -2,6 +2,7 @@ package org.bedu.sesion01.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.bedu.sesion01.model.Interviewer;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,5 +21,19 @@ public class InterviewerRepositoryTest {
     int count = repository.count();
 
     assertEquals(count, 0);
+  }
+
+  @Test
+  @DisplayName("Repository should add an interviewer")
+  public void addInterviewerCase() {
+    String name = "Juan Perez";
+    String email = "juan@mail.com";
+    
+    Interviewer saved = repository.save(name, email);
+
+    assertEquals(1, saved.getId());
+    assertEquals(name, saved.getName());
+    assertEquals(email, saved.getEmail());
+    assertEquals(1, repository.count());
   }
 }
